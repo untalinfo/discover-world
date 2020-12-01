69f36feedc48678052063acdf1cb6463986b8122
Oskar Info <racso1607@gmail.com>
2020-12-01 11:44:55 -0500

    creacion del contenedor que almacenara los filtros

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components'
 import Header from '../components/Header';
 import Footer from '../components/Footer'
 import SearchBar from '../components/SearchBar'
+
+const ContainerFilters = styled.section`
+    width: 60vw;
+    height: 50px;
+    border: solid black;
+    margin: auto;
+    display: flex;
+`
 
 function HomePage(): JSX.Element {
     return (
         <>
             <Header />
             <SearchBar />
+            <ContainerFilters></ContainerFilters>
             <Footer />
         </>
     )
