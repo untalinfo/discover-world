f79c91d1a3a3ca697b0bedce58fb9491c2620558
Oskar Info <racso1607@gmail.com>
2020-12-02 23:23:50 -0500

    Se termina de hacer el filtro de cada una de las opciones

--- src/components/ListCountries.tsx
+++ src/components/ListCountries.tsx
@@ -1,28 +1,123 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
 type country = {
-    name: string
-}
+    name: string;
+    officialLanguages: [];
+    currencies: [];
+    subregion: {
+        name: string;
+        region: {
+            name: string;
+        };
+    };
+    [x: string]: any;
+};
 
 interface IProps {
-    countries: Array<country>
+    countries: Array<country>;
+    filters: {
+        language?: string;
+        region?: string;
+        currency?: string;
+    };
 }
 
+const hasLanguage = (country: country, filterlanguage: string): boolean => {
+    const { officialLanguages = [] } = country;
+    let result = officialLanguages.find((oLanguage: { name: string }) => {
+        return filterlanguage === oLanguage.name;
+    });
+
+    return result ? true : false;
+};
+
+const hasCurrency = (country: country, filterCurrency: string): boolean => {
+    const { currencies = [] } = country;
+    let result = currencies.find((currency: { name: string }) => {
+        return filterCurrency === currency.name;
+    });
+
+    return result ? true : false;
+};
+
+const isRegion = (country: country, filterRegion: string): boolean => {
+        let result;
+    try {
+        result = country.subregion.region.name === filterRegion;
+
+    } catch (error) {
+        result = false
+    }   
+
+    return result;
+};
+
 function ListCountries(props: IProps) {
+    const {
+        filters,
+        filters: { language = '', currency = '', region = '' },
+        countries,
+    } = props;
 
-    const { countries } = props;
-    console.log(countries)
-    const Countries = () => countries.map((country: country) => {
-        return <Link to="/country"><span>{country.name}</span></Link>
-    })
+    const [ filteredCountries, setFilteredCountries ] = useState<Array<country>>([])
 
-    return (
-        <>
-            {Countries()}
-        </>
-    )
+    const filterCountries = (countries: Array<country>) => {
+
+
+        if(!language && !region && !currency){
+            
+            return countries;
+        } 
+
+        let byLanguage: Array<country> = [];
+        let byRegion: Array<country> = [];
+        let byCurrency: Array<country> = [];
+        let toIntersect = [];
+
+        countries.forEach((country: country) => {
+            
+            if(language && hasLanguage(country,language)){
+                
+                byLanguage.push(country)
+                
+            }
+            if(currency && hasCurrency(country,currency)){
+                byCurrency.push(country)
+                
+            }
+            if(region && isRegion(country,region)){
+                byRegion.push(country)
+    
+            }
+
+        });
+
+        if(language) toIntersect.push(byLanguage)
+        if(region) toIntersect.push(byRegion)
+        if(currency) toIntersect.push(byCurrency)
+
+        let result  = _.intersection(...toIntersect);
+
+        return result;
+
+    };
+
+    useEffect(() => {
+        setFilteredCountries(filterCountries(countries))
+    }, [filters, countries]);
+
+    const Countries = () =>
+        filteredCountries.map((country: country, index: number) => {
+            return (
+                <Link to="/country" key={index}>
+                    <span>{country.name}</span>
+                </Link>
+            );
+        });
+
+    return <>{Countries()}</>;
 }
 
 export default ListCountries;
--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Header from '../components/Header';
-import Footer from '../components/Footer'
-import SearchBar from '../components/SearchBar'
-import ListCountries from '../components/ListCountries'
+import Footer from '../components/Footer';
+import SearchBar from '../components/SearchBar';
+import ListCountries from '../components/ListCountries';
 import { useQuery, gql } from '@apollo/client';
 import Filter from '../components/Filter';
-
 
 const desktop = `@media (min-width: 1024px)`;
 const tablet = `@media (min-width: 600px)`;
@@ -18,10 +17,10 @@ const ContainerFilters = styled.section`
     margin: auto;
     display: flex;
     justify-content: space-between;
-`
+`;
 const ContainerDisplay = styled.main`
     width: 40vw;
-    height: 50vh;
+    height: 45vh;
     overflow-y: scroll;
     margin: auto;
     text-align: center;
@@ -30,7 +29,7 @@ const ContainerDisplay = styled.main`
         font-size: 4rem;
     }
 
-    a{
+    a {
         text-decoration: none;
         font-size: 1rem;
         display: block;
@@ -51,11 +50,10 @@ const ContainerDisplay = styled.main`
             font-size: 3rem;
         }
     }
-    
 `;
 
 const WORLD_DATA = gql`
-query {
+    query {
         Region {
             name
         }
@@ -66,76 +64,103 @@ query {
             name
         }
         Country {
-        name
-        alpha2Code
-        area
-        population
-        capital
-        subregion {
             name
-            region {
+            alpha2Code
+            area
+            population
+            capital
+            subregion {
+                name
+                region {
+                    name
+                }
+            }
+            officialLanguages {
                 name
             }
-        }
-        officialLanguages {
-            name
-        }
-        currencies {
-            name
-        }
-        flag {
-            emoji
+            currencies {
+                name
+            }
+            flag {
+                emoji
+            }
         }
     }
-}
 `;
 
+interface IFiltersState {
+    language: string;
+    currency: string;
+    region: string;
+}
+
 function HomePage(props: any): JSX.Element {
-
-
+    
     const [countries, setCountries] = useState<[]>([]);
     const [languageOptions, setLanguageOptions] = useState<[]>([]);
     const [regionOptions, setRegionOptions] = useState<[]>([]);
     const [currencyOptions, setCurrencyOptions] = useState<[]>([]);
-    const [filters, setFilters] = useState<any>({ language: '', currency: '', region: '' });
+    const [filters, setFilters] = useState<IFiltersState>({
+        language: '',
+        currency: '',
+        region: '',
+    });
 
-    const { loading, data, error } = useQuery(WORLD_DATA)
+    const { loading, data, error } = useQuery(WORLD_DATA);
 
     useEffect(() => {
         if (data) {
-            setCountries(data.Country)
-            setLanguageOptions(data.Language)
-            setCurrencyOptions(data.Currency)
-            setRegionOptions(data.Region)
+            setCountries(data.Country);
+            setLanguageOptions(data.Language);
+            setCurrencyOptions(data.Currency);
+            setRegionOptions(data.Region);
         }
-    }, [data])
+    }, [data]);
 
     const filterSelected = (filterName: string, value: string) => {
-
-
-        filters[filterName] = value;
-        setFilters(filters)
-        console.log(filters)
-        
-
-    }
+    
+        setFilters((prevState) => ({
+            ...prevState,
+            [filterName]: value,
+        }));
+    };
 
     return (
         <>
             <Header />
             <SearchBar />
             <ContainerFilters>
-                <Filter name={'Language'} options={languageOptions} onSelect={filterSelected} />
-                <Filter name={'Currency'} options={currencyOptions}  onSelect={filterSelected} />
-                <Filter name={'Region'} options={regionOptions}  onSelect={filterSelected}/>
+                <Filter
+                    name={'Language'}
+                    options={languageOptions}
+                    onSelect={filterSelected}
+                />
+                <Filter
+                    name={'Currency'}
+                    options={currencyOptions}
+                    onSelect={filterSelected}
+                />
+                <Filter
+                    name={'Region'}
+                    options={regionOptions}
+                    onSelect={filterSelected}
+                />
             </ContainerFilters>
             <ContainerDisplay>
-                {loading ? <p>Loading...</p> : (!error) && <ListCountries countries={countries} />}
-
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    !error && (
+                        <ListCountries
+                            countries={countries}
+                            filters={filters}
+                        />
+                    )
+                )}
             </ContainerDisplay>
             <Footer />
         </>
-    )
+    );
 }
 
 export default HomePage;
